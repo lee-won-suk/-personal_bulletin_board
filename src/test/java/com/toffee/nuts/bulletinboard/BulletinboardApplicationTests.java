04f76b1b0e29441a8a7a1c9b02864037cdee652c
leewonsuk <tjdjdjrdl123@naver.com>
2023-01-11 23:41:59 +0900

    [기능] first commit

---
+++ src/test/java/com/toffee/nuts/bulletinboard/BulletinboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toffee.nuts.bulletinboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
