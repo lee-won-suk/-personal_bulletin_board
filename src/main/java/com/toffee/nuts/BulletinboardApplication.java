f47622781b12f41bc31e241802b462f9530a31cb
leewonsuk <tjdjdjrdl123@naver.com>
2023-01-14 14:14:32 +0900

    [기능] first commit

--- src/main/java/com/toffee/nuts/BulletinboardApplication.java
+++ src/main/java/com/toffee/nuts/BulletinboardApplication.java
@@ -1,4 +1,4 @@
-package com.toffee.nuts.bulletinboard;
+package com.toffee.nuts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/toffee/nuts/BulletinboardApplicationTests.java
+++ src/test/java/com/toffee/nuts/BulletinboardApplicationTests.java
@@ -1,4 +1,4 @@
-package com.toffee.nuts.bulletinboard;
+package com.toffee.nuts;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
