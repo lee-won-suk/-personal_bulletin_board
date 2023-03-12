31667ff2b20b5f92d32a37491694ccfaefab562f
leewonsuk <tjdjdjrdl123@naver.com>
2023-03-12 15:49:31 +0900

    [기능] 로그인관련 config 파일 및 클래스 파일 추가

--- src/main/java/com/toffee/nuts/bulletinboard/service/UserDetailServiceImpl.java
+++ src/main/java/com/toffee/nuts/bulletinboard/service/UserDetailServiceImpl.java
@@ -3,17 +3,22 @@ package com.toffee.nuts.bulletinboard.service;
 import com.toffee.nuts.bulletinboard.entity.MemberDetails;
 import com.toffee.nuts.bulletinboard.entity.User;
 import com.toffee.nuts.bulletinboard.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 
 /**
  *
  * loginProcessiongUrl 주소로 들어오면 UserDetailsService 구현 클래스내 loadUserByUsername이 진행.
  */
+
+@Slf4j
 @Service
 public class UserDetailServiceImpl implements UserDetailsService {
 
@@ -24,7 +29,11 @@ public class UserDetailServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
 
-        User user = userRepository.findByName(name);
+        User user = userRepository.findByName(name).orElseThrow(() -> {
+            log.info("존재하지 않는 name 입니다.");
+            return new UsernameNotFoundException("존재하지 않는 name 입니다.");
+        });
+
 
 
         return new MemberDetails(user);
